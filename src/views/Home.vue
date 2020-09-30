f27cf37c65fae70a475ae7d8f6d9f23fec66a0d1
rahmat-andika <massabeni30@gmail.com>
2020-09-30 22:19:38 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <Header/>
+    <Hero/>
+    <Product/>
+    <Instagram/>
+    <Logo/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '../components/Header.vue';
+import Hero from '../components/Hero.vue';
+import Product from '../components/Product.vue';
+import Instagram from '../components/Instagram.vue';
+import Logo from '../components/Logo.vue';
+import Footer from '../components/Footer.vue';
+
+export default {
+  name: 'Home',
+  components: {
+      Header,
+      Hero,
+      Product,
+      Instagram,
+      Logo,
+      Footer
+  },
+}
+</script>
